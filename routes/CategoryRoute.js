cd83093ab5a040c077e6c42fe59662567cbd908e
hanialazreg <lazreghania@gmail.com>
2020-05-15 12:17:52 +0100

    refactoring category route backend

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,53 +1,56 @@
 const mongoose = require("mongoose");
 const Category = require("../Database/Category ");
-module.exports = app => {
-    app.post('/api/AddCategory', (req, res) => {
-
-        Category.createCategory(req.body, (err, data) => {
-            if (err) {
-                res.status(404).send(err)
-                console.log('duplicate key')
-            } else {
-                res.status(200).send(data)
-            }
-        })
-    })
-
-
-    app.delete("/api/deleteCategory", (req, res) => {
-        Category.deleteCategory(req.body.name, (err, result) => {
-            if (result) {
-                res.status(200).send("true");
-            } else {
-                res.status(404).send("false");
-            }
-        });
+module.exports = (app) => {
+  ///
+  const addCat = (req, res) => {
+    Category.createCategory(req.body, (err, data) => {
+      if (err) {
+        res.status(404).send(err);
+        console.log("duplicate key");
+      } else {
+        res.status(200).send(data);
+      }
     });
+  };
 
-    app.get("/api/getProducts", (req, res) => {
-
-        console.log(req.query.category);
-        Category.getAllProductByCategory(req.query.category, (err, data) => {
-            if (err) {
-                res.status(401).send(err);
-            } else {
-                res.status(200).send(data);
-            }
-        });
+  const deleteCat = (req, res) => {
+    Category.deleteCategory(req.body.name, (err, result) => {
+      if (result) {
+        res.status(200).send("true");
+      } else {
+        res.status(404).send("false");
+      }
     });
-
-
-
-    app.get("/api/categories", (req, res) => {
-        Category.getAll((err, data) => {
-            if (err) {
-                res.status(404).send(err);
-            } else {
-                res.status(200).send(data);
-                res.end();
-            }
-        });
-
+  };
+  const getProductByCat = (req, res) => {
+    console.log(req.query.category);
+    Category.getAllProductByCategory(req.query.category, (err, data) => {
+      if (err) {
+        res.status(401).send(err);
+      } else {
+        res.status(200).send(data);
+      }
     });
-}
+  };
 
+  const getAllCat = (req, res) => {
+    Category.getAll((err, data) => {
+      if (err) {
+        res.status(404).send(err);
+      } else {
+        res.status(200).send(data);
+        res.end();
+      }
+    });
+  };
+
+  ///
+
+  app.post("/api/AddCategory", addCat);
+
+  app.delete("/api/deleteCategory", deleteCat);
+
+  app.get("/api/getProducts", getProductByCat);
+
+  app.get("/api/categories", getAllCat);
+};
