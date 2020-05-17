162da37d363bab9bc38ac2c6740234c9da035c71
hanialazreg <lazreghania@gmail.com>
2020-05-17 23:54:52 +0100

    cloudinary params

---
+++ helpers/cloudinary.js
@@ -0,0 +1,6 @@
+const cloudinary = (module.exports = require("cloudinary"));
+cloudinary.config({
+  cloud_name: "dxgzln4cr",
+  api_key: "836666166545917",
+  api_secret: "jeQG1kWivGANuM1qgaEKkT-bInw",
+});
